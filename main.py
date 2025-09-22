3ad800ce2c15c1e453948e3b561ebf6f7b8a4f06
TrulsElg <trulseb@gmail.com>
2025-09-22 13:37:46 +0200

    Change content to a list of messages, ie. a conversation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,10 +12,14 @@ client = genai.Client(api_key=api_key)
 
 def main():
     if len(sys.argv) > 1:
-        prompt = sys.argv[1]
+        user_prompt = sys.argv[1]
+
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
 
         response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents=prompt
+            model='gemini-2.0-flash-001', contents=messages
         )
         print(response.text)
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
