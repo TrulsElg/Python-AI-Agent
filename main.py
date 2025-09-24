597020c4737c724f6e7de37da5166bbed32900da
TrulsElg <trulseb@gmail.com>
2025-09-24 16:41:06 +0200

    Alter model call to include system_prompt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from config import system_prompt, model_name
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -23,7 +24,9 @@ def main():
         ]
 
         response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents=messages
+            model= model_name,
+            contents=messages,
+            config=types.GenerateContentConfig(system_instruction=system_prompt)
         )
 
         if verbose_output:
